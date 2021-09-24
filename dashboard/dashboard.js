c8e009208794a0c3b8afd4b4b623111f58b6df20
Glitch (south-innate-rat) <none>
2021-09-24 16:06:53 +0000

    🌁🍓 Checkpoint ./dashboard/dashboard.js:35098771/46574

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -15,7 +15,7 @@ const app = express();
 const MemoryStore = require("memorystore")(session);
 
 // We export the dashboard as a function which we call in ready event.
-module.exports = async (client) => {
+module.exports = async client => {
   // We declare absolute paths.
   const dataDir = path.resolve(`${process.cwd()}${path.sep}dashboard`); // The absolute path of current this directory.
   const templateDir = path.resolve(`${dataDir}${path.sep}templates`); // Absolute path of ./templates directory.
@@ -27,10 +27,10 @@ module.exports = async (client) => {
   // Validating the url by creating a new instance of an Url then assign an object with the host and protocol properties.
   // If a custom domain is used, we take the protocol, then the hostname and then we add the callback route.
   // Ex: Config key: https://localhost/ will have - hostname: localhost, protocol: http
-  
+
   var callbackUrl;
   var domain;
-  
+
   try {
     const domainUrl = new URL(process.env.domain);
     domain = {
@@ -41,42 +41,54 @@ module.exports = async (client) => {
     console.log(e);
     throw new TypeError("Invalid domain specific in the config file.");
   }
-  
+
   if (process.env.usingCustomDomain) {
-    callbackUrl =  `${domain.protocol}//${domain.host}/callback`
+    callbackUrl = `${domain.protocol}//${domain.host}/callback`;
   } else {
-    callbackUrl = `${domain.protocol}//${domain.host}${process.env.port == 80 ? "" : `:${process.env.port}`}/callback`;
+    callbackUrl = `${domain.protocol}//${domain.host}${
+      process.env.port == 80 ? "" : `:${process.env.port}`
+    }/callback`;
   }
-  
+
   // This line is to inform users where the system will begin redirecting the users.
   // And can be removed.
   console.log("===");
-  console.log(`Info: Make sure you have added the following url to the discord's OAuth callback url section in the developer portal:\n${callbackUrl}`);
+  console.log(
+    `Info: Make sure you have added the following url to the discord's OAuth callback url section in the developer portal:\n${callbackUrl}`
+  );
   console.log("===");
 
   // We set the passport to use a new discord strategy, we pass in client id, secret, callback url and the scopes.
   /** Scopes:
    *  - Identify: Avatar's url, username and discriminator.
    *  - Guilds: A list of partial guilds.
-  */
-  passport.use(new Strategy({
-    clientID: process.env.id,
-    clientSecret: process.env.clientSecret,
-    callbackURL: callbackUrl,
-    scope: ["identify", "guilds"]
-  },
-  (accessToken, refreshToken, profile, done) => { // eslint-disable-line no-unused-vars
-    // On login we pass in profile with no logic.
-    process.nextTick(() => done(null, profile));
-  }));
+   */
+  passport.use(
+    new Strategy(
+      {
+        clientID: process.env.id,
+        clientSecret: process.env.clientSecret,
+        callbackURL: callbackUrl,
+        scope: ["identify", "guilds"]
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // eslint-disable-line no-unused-vars
+        // On login we pass in profile with no logic.
+        process.nextTick(() => done(null, profile));
+      }
+    )
+  );
 
   // We initialize the memorystore middleware with our express app.
-  app.use(session({
-    store: new MemoryStore({ checkPeriod: 86400000 }),
-    secret: "#@%#&^$^$%@$^$&%#$%@#$%$^%&$%^#$%@#$%#E%#%@$FEErfgr3g#%GT%536c53cc6%5%tv%4y4hrgrggrgrgf4n",
-    resave: false,
-    saveUninitialized: false,
-  }));
+  app.use(
+    session({
+      store: new MemoryStore({ checkPeriod: 86400000 }),
+      secret:
+        "#@%#&^$^$%@$^$&%#$%@#$%$^%&$%^#$%@#$%#E%#%@$FEErfgr3g#%GT%536c53cc6%5%tv%4y4hrgrggrgrgf4n",
+      resave: false,
+      saveUninitialized: false
+    })
+  );
 
   // We initialize passport middleware.
   app.use(passport.initialize());
@@ -85,43 +97,45 @@ module.exports = async (client) => {
   // We bind the domain.
   app.locals.domain = process.env.domain.split("//")[1];
 
-
-app.set('view engine', 'ejs');
-
+  app.set("view engine", "ejs");
 
   // We initialize body-parser middleware to be able to read forms.
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
 
-  
   // We host all of the files in the assets using their name in the root address.
   // A style.css file will be located at http://<your url>/style.css
   // You can link it in any template using src="/assets/filename.extension"
   app.use("/", express.static(path.resolve(`${dataDir}${path.sep}assets`)));
-  
+
   // We declare a renderTemplate function to make rendering of a template in a route as easy as possible.
   const renderTemplate = (res, req, template, data = {}) => {
-    
     const botInfo = {
-    username: client.user.username,
-    status: client.user.presence.status,
-    users: client.users.size,
-    guilds: client.guilds.size
-  };
-    
-    const guilds = []
+      username: client.user.username,
+      status: client.user.presence.status,
+      users: client.users.size,
+      guilds: client.guilds.size
+    };
+
+    const guilds = [];
     if (req.user && data.perms) {
-    req.user.guilds.forEach(guild => {
-      const permsOnGuild = new data.perms(guild.permissions);
-      if(!permsOnGuild.has("MANAGE_GUILD") || !client.guilds.cache.get(guild.id)) return;
-      
-      guilds.push(guild)
-    })
+      req.user.guilds.forEach(guild => {
+        const permsOnGuild = new data.perms(guild.permissions);
+        if (
+          !permsOnGuild.has("MANAGE_GUILD") ||
+          !client.guilds.cache.get(guild.id)
+        )
+          return;
+
+        guilds.push(guild);
+      });
     }
-    
-    // Default base data which passed to the ejs template by default. 
+
+    // Default base data which passed to the ejs template by default.
     const baseData = {
       bot: client,
       botInfo: botInfo,
@@ -130,7 +144,10 @@ app.set('view engine', 'ejs');
       guilds: req.isAuthenticated() ? guilds : null
     };
     // We render template using the absolute path of the template and the merged default data with the additional data provided.
-    res.render(path.resolve(`${templateDir}${path.sep}${template}`), Object.assign(baseData, data));
+    res.render(
+      path.resolve(`${templateDir}${path.sep}${template}`),
+      Object.assign(baseData, data)
+    );
   };
 
   // We declare a checkAuth function middleware to check if an user is logged in or not, and if not redirect him.
@@ -141,7 +158,7 @@ app.set('view engine', 'ejs');
     req.session.backURL = req.url;
     // We redirect user to login endpoint/route.
     res.redirect("/login");
-  }
+  };
 
   // Login endpoint.
   app.get("/login", (req, res, next) => {
@@ -157,55 +174,50 @@ app.set('view engine', 'ejs');
       req.session.backURL = "/";
     }
     // Forward the request to the passport middleware.
-    res.redirect("https://api.dismic.org/v1/auth/login")
-  },
-  )
+    res.redirect("https://api.dismic.org/v1/auth/login");
+  });
 
   // Callback endpoint.
-  app.get("/callback", passport.authenticate("discord", { failureRedirect: "/" }), /* We authenticate the user, if user canceled we redirect him to index. */ (req, res) => {
-    // If user had set a returning url, we redirect him there, otherwise we redirect him to index.
-    if (req.session.backURL) {
-      const url = req.session.backURL;
-      req.session.backURL = null;
-      res.redirect(url);
-    } else {
-      res.redirect("/");
+  app.get(
+    "/callback",
+    passport.authenticate("discord", { failureRedirect: "/" }),
+    /* We authenticate the user, if user canceled we redirect him to index. */ (
+      req,
+      res
+    ) => {
+      // If user had set a returning url, we redirect him there, otherwise we redirect him to index.
+      if (req.session.backURL) {
+        const url = req.session.backURL;
+        req.session.backURL = null;
+        res.redirect(url);
+      } else {
+        res.redirect("/");
+      }
     }
-  });
+  );
 
   // Logout endpoint.
-  app.get("/logout", function (req, res) {
-    // We destroy the session.
-    req.session.destroy(() => {
-      // We logout the user.
-      req.logout();
-      // We redirect user to index.
-      res.redirect("/");
-    });
-  });
 
   // Index endpoint.
   app.get("/", (req, res) => {
     renderTemplate(res, req, "index.ejs");
   });
-  
-  app.get("/support", (req, res) => {
-    res.redirect("https://discord.gg/invite/95QvjUn9pd")
-  });
 
+  app.get("/support", (req, res) => {
+    res.redirect("https://discord.gg/invite/95QvjUn9pd");
+  });
 
   // Dashboard endpoint.
   app.get("/guilds", checkAuth, (req, res) => {
     renderTemplate(res, req, "guilds.ejs", { perms: Discord.Permissions });
   });
-  
+
   app.get("/commands", checkAuth, (req, res) => {
     renderTemplate(res, req, "commands.ejs", { perms: Discord.Permissions });
   });
 
   app.get("/guild/:guildID", checkAuth, async (req, res) => {
-     res.redirect("/guild/" + req.params.guildID + "/general")
-    
+    res.redirect("/guild/" + req.params.guildID + "/general");
   });
   // Settings endpoint.
   app.get("/guild/:guildID/general", checkAuth, async (req, res) => {
@@ -213,10 +225,14 @@ app.set('view engine', 'ejs');
     const guild = client.guilds.cache.get(req.params.guildID);
     if (!guild) return res.redirect("/guilds");
     const member = guild.members.cache.get(req.user.id);
-    if(!member){
-      try{ await guild.members.fetch();
+    if (!member) {
+      try {
+        await guild.members.fetch();
         member = guild.members.cache.get(req.user.id);
-      } catch (err) { console.error(`Couldn't fetch the members of ${guild.id}: ${err}`); }}
+      } catch (err) {
+        console.error(`Couldn't fetch the members of ${guild.id}: ${err}`);
+      }
+    }
     if (!member) return res.redirect("/guilds");
     if (!member.permissions.has("MANAGE_GUILD")) return res.redirect("/guilds");
 
@@ -227,40 +243,66 @@ app.set('view engine', 'ejs');
       const newSettings = new GuildSettings({
         gid: guild.id
       });
-      await newSettings.save().catch(()=>{});
+      await newSettings.save().catch(() => {});
       storedSettings = await GuildSettings.findOne({ gid: guild.id });
     }
-  
-    renderTemplate(res, req, "general.ejs", { guild, settings: storedSettings, alert: null });
+
+    renderTemplate(res, req, "general.ejs", {
+      guild,
+      settings: storedSettings,
+      alert: null
+    });
   });
 
-    // Settings endpoint.
-    app.post("/guild/:guildID/general", checkAuth, async (req, res) => {
-        // We validate the request, check if guild exists, member is in guild and if member has minimum permissions, if not, we redirect it back.
-        const guild = client.guilds.cache.get(req.params.guildID);
-        if (!guild) return res.redirect("/guilds");
-        const member = guild.members.cache.get(req.user.id);
-        if (!member) return res.redirect("/guilds");
-        if (!member.permissions.has("MANAGE_GUILD")) return res.redirect("/guilds");
-        // We retrive the settings stored for this guild.
-        var storedSettings = await GuildSettings.findOne({ gid: guild.id });
-        if (!storedSettings) {
-          // If there are no settings stored for this guild, we create them and try to retrive them again.
-          const newSettings = new GuildSettings({
-            gid: guild.id
-          });
-          await newSettings.save().catch(()=>{});
-          storedSettings = await GuildSettings.findOne({ gid: guild.id });
-        }
-      
-        // We set the prefix of the server settings to the one that was sent in request from the form.
-        storedSettings.prefix = req.body.prefix;
-        // We save the settings.
-        await storedSettings.save().catch(() => {});
+  // Settings endpoint.
+  app.post("/guild/:guildID/general", checkAuth, async (req, res) => {
+    // We validate the request, check if guild exists, member is in guild and if member has minimum permissions, if not, we redirect it back.
+    const guild = client.guilds.cache.get(req.params.guildID);
+    if (!guild) return res.redirect("/guilds");
+    const member = guild.members.cache.get(req.user.id);
+    if (!member) return res.redirect("/guilds");
+    if (!member.permissions.has("MANAGE_GUILD")) return res.redirect("/guilds");
+    // We retrive the settings stored for this guild.
+    var storedSettings = await GuildSettings.findOne({ gid: guild.id });
+    if (!storedSettings) {
+      // If there are no settings stored for this guild, we create them and try to retrive them again.
+      const newSettings = new GuildSettings({
+        gid: guild.id
+      });
+      await newSettings.save().catch(() => {});
+      storedSettings = await GuildSettings.findOne({ gid: guild.id });
+    }
 
-        // We render the template with an alert text which confirms that settings have been saved.
-        renderTemplate(res, req, "general.ejs", { guild, settings: storedSettings, alert: "Your settings have been saved." });
+    // We set the prefix of the server settings to the one that was sent in request from the form.
+    storedSettings.prefix = req.body.prefix;
+    // We save the settings.
+    await storedSettings.save().catch(() => {});
+
+    // We render the template with an alert text which confirms that settings have been saved.
+    renderTemplate(res, req, "general.ejs", {
+      guild,
+      settings: storedSettings,
+      alert: "Your settings have been saved."
     });
+  });
 
-  app.listen(process.env.port, null, null, () => console.log(`Dashboard is up and running on port ${process.env.port}.`));
+  app.get("/logout", function(req, res) {
+    // We destroy the session.
+    req.session.destroy(() => {
+      // We logout the user.
+      req.logout();
+      // We redirect user to index.
+      res.redirect("/");
+    });
+  });
+
+  app.get("*", function(req, res) {
+    renderTemplate(res, req, "general.ejs", {
+      currentURL: `${req.protocol}://${req.get("host")}${req.originalUrl}`
+    });
+  });
+
+  app.listen(process.env.port, null, null, () =>
+    console.log(`Dashboard is up and running on port ${process.env.port}.`)
+  );
 };
